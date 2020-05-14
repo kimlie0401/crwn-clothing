671066b86c7cb1177b9c9671f83dc5f021f0e9fe
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-05-13 17:04:06 -0700

    108. Cart Component

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(...middlewares),
+    window.navigator.userAgent.includes("Chrome")
+      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+          window.__REDUX_DEVTOOLS_EXTENSION__()
+      : compose
+  )
+);
 
 export default store;
