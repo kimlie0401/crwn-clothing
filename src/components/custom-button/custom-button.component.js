86a67344e237ca59650fce6349fcae1467ea3edb
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-05-22 15:03:51 -0700

    156. styled-components In Our App 3

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -1,21 +1,12 @@
 import React from "react";
 
-import "./custom-button.styles.scss";
+import { CustomButtonContainer } from "./custom-button.styles";
+// import "./custom-button.styles.scss";
 
-const CustomButton = ({
-  children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
-}) => (
-  <button
-    className={`${inverted ? "inverted" : ""} ${
-      isGoogleSignIn ? "google-sign-in" : ""
-    } custom-button`}
-    {...otherProps}
-  >
-    {children}
-  </button>
+const CustomButton = (props) => (
+  <CustomButtonContainer className="custom-button" {...props}>
+    {props.children}
+  </CustomButtonContainer>
 );
 
 export default CustomButton;
