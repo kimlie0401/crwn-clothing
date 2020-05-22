78a74a23f5c4cd33a1ae3462f8b52fa8e553d154
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-05-21 17:50:36 -0700

    153. styled-components In Our App

---
+++ src/components/header/header.styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+export const HeaderContainer = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`
+
+export const LogoContainer = 
