86e91d29c1842a163a481c05654d6c06bb9e35d0
kimlie0401 <54561489+kimlie0401@users.noreply.github.com>
2020-05-23 10:15:50 -0700

    # Styled Component Done

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -3,13 +3,13 @@ import React from "react";
 import SingIn from "../../components/sign-in/sign-in.component";
 import SingUp from "../../components/sign-up/sign-up.component";
 
-import "./sign-in-and-sign-up.styles.scss";
+import { SignInAndSignUpContainer } from "./sign-in-and-sign-up.styles";
 
 const SignInAndSignUpPage = () => (
-  <div className="sign-in-and-sign-up">
+  <SignInAndSignUpContainer>
     <SingIn />
     <SingUp />
-  </div>
+  </SignInAndSignUpContainer>
 );
 
 export default SignInAndSignUpPage;
